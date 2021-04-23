6fb45b11ecabf43b5bd8c0e62243907e5888e047
Yovanny <yovannymontelongo@gmail.com>
2021-04-23 18:55:08 -0500

    initial setup of letters, numbers, and symbols

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+numOfLetters = int(input("How many letter would you like?\n"))
+numOfSymbols = int(input(f"How many symbols would you like?\n"))
+numOfNumbers = int(input(f"How many numbers would you like?\n"))
